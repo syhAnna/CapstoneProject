63e94878f0ccb8efdbd42fd0568b1b5137314083
Yuhan Shao <shaoyuhan96117@gmail.com>
2022-11-09 16:17:11 -0800

    debug sim_matrix function

--- my_model_sim_matrix.py
+++ my_model_sim_matrix.py
@@ -20,22 +20,23 @@ def annoyID(sequences, fname):
     annoy.save(MODEL_ROOT + fname)
 
 
-def sim_matrix(texts, annoy_fname):
+def sim_matrix(texts, sequences, annoy_fname):
     """
     Construct similarity matrix through annoy
     """
-    texts_len, max_input_len = len(texts), max([len(x) for x in texts])
-    sim_matrix = (-1) * np.ones((texts_len, texts_len))
+
+    texts_len, max_input_len = len(texts), max([len(x) for x in sequences])
+    sim_mat = (-1) * np.ones((texts_len, texts_len))
     annoy = AnnoyIndex(max_input_len, 'angular')
     annoy.load(annoy_fname)
 
     for i in tqdm(range(texts_len)):
         neighbor_id, dist = annoy.get_nns_by_item(i, texts_len, include_distances=True)
         sim_row = (-1) * np.ones((texts_len,))
-        for j in range(texts_len):
+        for j in range(len(neighbor_id)):
             sim_row[neighbor_id[j]] = dist[j]
-        sim_matrix[i, :] = sim_row.T
-    return sim_matrix
+        sim_mat[i, :] = sim_row.T
+    return sim_mat
 
 
 if __name__ == '__main__':
@@ -47,8 +48,8 @@ if __name__ == '__main__':
     fname1, fname2 = '/my_model_input_annoy.ann', '/my_model_target_annoy.ann'
     annoyID(input_sequences, fname1)
     annoyID(target_sequences, fname2)
-    input_sim_matrix = sim_matrix(input_texts, MODEL_ROOT + fname1)
-    target_sim_matrix = sim_matrix(target_texts, MODEL_ROOT + fname2)
+    input_sim_matrix = sim_matrix(input_texts, input_sequences, MODEL_ROOT + fname1)
+    target_sim_matrix = sim_matrix(target_texts, target_sequences, MODEL_ROOT + fname2)
     saveData(input_sim_matrix, MODEL_ROOT + '/input_sim_matrix.pickle')
     saveData(target_sim_matrix, MODEL_ROOT + '/target_sim_matrix.pickle')
     input_sim_df = pd.DataFrame(input_sim_matrix, columns=range(input_sim_matrix.shape[1]))
